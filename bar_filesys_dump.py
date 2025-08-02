648e32f10be8d1e6706f762b622d4c88b39c69dc
SynaMax <djsynamax@gmail.com>
2025-08-02 01:47:23 -0400

    Groovy!

---
+++ bar_filesys_dump.py
@@ -0,0 +1,354 @@
+## Beetle Adventure Racing! Filesys Dump by SynaMax
+##
+## Based on previous work on Pilotwings 64 by queueRAM
+## https://github.com/queueRAM/pilotwings_64/blob/master/pw64_filesys_dump.py
+
+
+#!/usr/bin/env python
+
+import argparse
+import binascii
+import struct
+import sys
+
+def auto_int(x):
+    return int(x, 0)
+
+def decompress_mio0(raw_bytes):
+    magic = raw_bytes[:4]
+
+    assert magic == b'MIO0'
+
+    uncompressed_size, lengths_offs, data_offs = struct.unpack('>LLL', raw_bytes[4:16])
+    flags_offs = 0x10
+
+    output = b""
+    while True:
+        command_byte = raw_bytes[flags_offs]
+        flags_offs += 1
+
+        for i in reversed(range(8)):
+            if command_byte & (1 << i):
+                # Literal
+                uncompressed_size -= 1
+                output += bytes([raw_bytes[data_offs]])
+                data_offs += 1
+            else:
+                # LZSS
+                tmp, = struct.unpack('>H', raw_bytes[lengths_offs:lengths_offs+2])
+                lengths_offs += 2
+
+                window_offset = (tmp & 0x0FFF) + 1
+                window_length = (tmp >> 12) + 3
+                uncompressed_size -= window_length
+                for j in range(window_length):
+                    output += bytes([output[-window_offset]])
+
+            if uncompressed_size <= 0:
+                return output
+
+def print_adat_decoded(hex_data):
+    # The DATA blocks in the ADAT container appear to be "coded" ASCII strings.
+    # The strings use a sort of look-up table as seen below.
+    # This was probably done for easier localization (Kanji font textures?)
+    # The Font Sprite/Texture maps are in the "STRG" container/blocks.
+    # This table was extrapolated from the FS dump and PJ64 memory searches.
+    char_map_combined = { # // Normal Font //
+                          '00': '0', '01': '1', '02': '2', '03': '3', '04': '4',
+                          '05': '5', '06': '6', '07': '7', '08': '8', '09': '9',
+                          '0A': 'A', '0B': 'B', '0C': 'C', '0D': 'D', '0E': 'E',
+                          '0F': 'F', '10': 'G', '11': 'H', '12': 'I', '13': 'J',
+                          '14': 'K', '15': 'L', '16': 'M', '17': 'N', '18': 'O',
+                          '19': 'P', '1A': 'Q', '1B': 'R', '1C': 'S', '1D': 'T',
+                          '1E': 'U', '1F': 'V', '20': 'W', '21': 'X', '22': 'Y',
+                          '23': 'Z', '24': 'a', '25': 'b', '26': 'c', '27': 'd',
+                          '28': 'e', '29': 'f', '2A': 'g', '2B': 'h', '2C': 'i',
+                          '2D': 'j', '2E': 'k', '2F': 'l', '30': 'm', '31': 'n',
+                          '32': 'o', '33': 'p', '34': 'q', '35': 'r', '36': 's',
+                          '37': 't', '38': 'u', '39': 'v', '3A': 'w', '3B': 'x',
+                          '3C': 'y', '3D': 'z', '3E': '-', '3F': '#', '40': '<',
+                          '41': '>', '42': ' ', '43': '\"', '44': '(', '45': ')',
+                          '46': '*', '47': '&', '48': ',', '49': '.', '4A': '/',
+                          '4B': '!', '4C': '?', '4D': '\'', '4E': '#', '4F': ':',
+                          '50': '0', '51': '1', '52': '2', '53': '3', '54': '4',
+                          '55': '5', '56': '6', '57': '7', '58': '8', '59': '9',
+                          '5A': '\\', '5B': '\\', '5C': '\\', '5D': '\\',
+                          '5E': '\\', '5F': '\\',
+                          # // Bold Font //
+                          '60': '0', '61': '1', '62': '2', '63': '3', '64': '4',
+                          '65': '5', '66': '6', '67': '7', '68': '8', '69': '9',
+                          '6A': 'A', '6B': 'B', '6C': 'C', '6D': 'D', '6E': 'E',
+                          '6F': 'F', '70': 'G', '71': 'H', '72': 'I', '73': 'J',
+                          '74': 'K', '75': 'L', '76': 'M', '77': 'N', '78': 'O',
+                          '79': 'P', '7A': 'Q', '7B': 'R', '7C': 'S', '7D': 'T',
+                          '7E': 'U', '7F': 'V', '80': 'W', '81': 'X', '82': 'Y',
+                          '83': 'Z', '84': 'a', '85': 'b', '86': 'c', '87': 'd',
+                          '88': 'e', '89': 'f', '8A': 'g', '8B': 'h', '8C': 'i',
+                          '8D': 'j', '8E': 'k', '8F': 'l', '90': 'm', '91': 'n',
+                          '92': 'o', '93': 'p', '94': 'q', '95': 'r', '96': 's',
+                          '97': 't', '98': 'u', '99': 'v', '9A': 'w', '9B': 'x',
+                          '9C': 'y', '9D': 'z', '9E': '-', '9F': '#', 'A0': '<',
+                          'A1': '>', 'A2': ' ', 'A3': '\"', 'A4': '(', 'A5': ')',
+                          'A6': '*', 'A7': '&', 'A8': ',', 'A9': '.', 'AA': '/',
+                          'AB': '!', 'AC': '?', 'AD': '\'', 'AE': '}', 'AF': ':',
+                          'B0': '0', 'B1': '1', 'B2': '2', 'B3': '3', 'B4': '4',
+                          'B5': '5', 'B6': '6', 'B7': '7', 'B8': '8', 'B9': '9',
+                          'BA': '\\', 'BB': '\\', 'BC': '\\', 'BD': '\\',
+                          'BE': '\\', 'BF': '\\' }
+
+    # Take the raw binary data and convert to Hex
+    hex_data = str(binascii.b2a_hex(hex_data),'ascii')
+
+    # Split input stream of characters into hex bytes
+    hex_split = [(hex_data[i:i+2]) for i in range(0, len(hex_data), 2)]
+
+    # There are various "command" codes that I haven't figured out yet.
+    # They are detected below.
+    # Special "turn on bold" command (until newline)?
+    #   00 fd | 00 d4 | 00 00 | 00 4a
+    #   00 fd | 00 69 | 00 00 | 00
+    # Turn off bold?
+    #   00 fd | 00 7d | 00 00
+    # Weird ">" arrow in Sound settings
+    #   00 fd | 00 b4 | 00 00 00
+
+    # Empty list for storing final string
+    adat_final_string = []
+
+    # Read a pair of hex bytes
+    for i in range(0, len(hex_split), 2):
+        hex_pair = hex_split[i:i+2]
+
+        char_byte1 = hex_pair[0].upper()
+        char_byte2 = hex_pair[1].upper()
+
+        if char_byte1 == '00':
+          if char_byte2 == 'CA':
+              # slash? '\' ?
+              pass
+          elif char_byte2 == 'D4':
+              # Unknown char
+              pass
+          elif char_byte2 == 'FE':
+              # Newline
+              adat_final_string.append('\n')
+          elif char_byte2 == 'FD':
+              # Tab?
+              pass
+          elif char_byte2 == 'FF':
+              # EOF/EOS
+              break
+          else:
+              adat_final_string.append(char_map_combined[char_byte2])
+        else:
+          # We found some weird control char in our pair?
+          adat_final_string.append('?0')
+
+    print('    --------- Decoded String ---------')
+    for line in "".join(adat_final_string).splitlines():
+        print ('    %s' % line)
+    print('    ----------------------------------')
+
+def print_hex_dump(raw_bytes):
+    count = 0
+    for b in raw_bytes:
+        if count % 16 == 0:
+            sys.stdout.write(' ' * 4)
+        sys.stdout.write('{:02x} '.format(b))
+        count += 1
+        if count % 16 == 0:
+            sys.stdout.write('\n')
+    if count % 16:
+        sys.stdout.write('\n')
+
+def pw64_dump_filesys(fname, startOffset, hexSize):
+    def hexdump(raw_bytes):
+        if hexSize > 0:
+            if len(raw_bytes) > hexSize:
+                raw_bytes = raw_bytes[:hexSize]
+            print_hex_dump(raw_bytes)
+
+    with open(fname, 'rb') as fin:
+        fin.seek(startOffset)
+        dumping = True
+        while (dumping):
+            fileOffset = fin.tell()
+            sys.stdout.write('0x%06X|%06X: ' % (fileOffset, fileOffset - startOffset))
+            magic = fin.read(4)
+            magic2 = fin.read(4)
+            magicInt = int.from_bytes(magic, byteorder='big')
+            magic2Int = int.from_bytes(magic2, byteorder='big')
+            #breakpoint()
+            if magicInt == 0:
+                print('00000000 [EOF]')
+                break
+            magicStr = magic.decode('utf-8')
+            if magicStr == 'FORM':
+                formLength = magic2Int
+                #formLength = int.from_bytes(fin.read(4), byteorder='big')
+                formEnd = fin.tell() + formLength
+                print('%s: 0x%06X (end: 0x%06X)' % (magicStr, formLength, formEnd))
+                while (fin.tell() < formEnd):
+                    fileOffset = fin.tell()
+                    sys.stdout.write('0x%06X|%06X: ' % (fileOffset, fileOffset - startOffset))
+                    magic = fin.read(4)
+                    #breakpoint()
+                    magicStr = magic.decode('utf-8')
+
+                    ### UV = "UltraVision" ###
+                    #
+                    # 0x237D0 = First FORM table contains the pointers to all the
+                    # FORM chunks in the ROM.  The pointers are offset by
+                    # the start of the NEXT 'FORM' header (0x25FD0).
+                    #
+                    # You can think of FORM #0 as a table of contents, while FORM #1
+                    # is the start of actual game data.
+                    #
+                    # Weird 'UVFT' Header:
+                    #
+                    #   For FORM #0 there's a UVFT header that's immediately followed
+                    #   by 'UVMO'. I'm not sure why this is because 'UVFT' is related
+                    #   to fonts.
+                    #   Regardless, these headers show that all the FORM chunks are
+                    #   arranged by their type.  These types are:
+                    #
+                    ### Code-related ###
+                    #
+                    # 'UVMO' = Modules
+                    #   'MODU' containers inside, which appear to have
+                    #   'CODE' chunks embedded.
+                    #
+                    # 'UVTT' = Track
+                    #   Has 'CODE', 'PNTS', 'MDBG', 'RELA'
+                    #
+                    # 'UVDS' = ??
+                    #   Has 'CODE'...could be debug or "dynamic set" related?
+                    #
+                    # 'UVTX' = Texture
+                    #   Presumably has MIO0 compressed texture data
+                    #
+                    # 'UVTR' = Terrain  
+                    #
+                    # 'UVCT' = Contour
+                    #
+                    # 'UVVL' = Volume
+                    #
+                    # 'UVMD' = Model
+                    #
+                    # 'UVAN' = Animation
+                    #
+                    # 'UVEN' = Environment
+                    #
+                    # 'UVRW' = Save Game? Referenced as "ufile" (user file?) 
+                    #
+                    # 'UVPX' = Particle Effects
+                    #
+                    # 'UVBT' = Blit??
+                    #   
+                    # 'UVTS' = Texture Sequence
+                    #- 'UVSQ' = Texture Sequence Data
+                    #
+                    ### Audio related ###
+                    #
+                    # 'UVSX' = Sound FX
+                    # 'UVMS' = Music Sequences
+                    #- 'SEQS' = Music MIDI Sequences
+                    #
+                    # 'UVMB' = Music Banks
+                    #- '.CTL' = N64 Soundbank format
+                    #- '.TBL' = N64 ADPCM Audio Wavetable
+                    #
+                    ### Font related ###
+                    #
+                    # 'UVFT' = This is related to Fonts
+                    #
+                    # 'STRG' = String
+                    #
+                    # 'BITM' = Bitmap
+                    #
+                    # 'IMAG' = Image
+                    #
+                    # 'FRMT' = Format
+                    
+                    ## Beetle Adventure Racing has two types of UVFT?! One DOES have length, while the other doesn't
+                    if magicStr in ['UVTT', 'UVDS', 'UVTX', 'UVTR',
+                                    'UVCT', 'UVVL', 'UVMD', 'UVAN',
+                                    'UVEN', 'UVRW', 'UVPX', 'UVBT',
+                                    'UVSX', 'UVMS', 'UVMB', 'UVTS',
+                                    'COMM', 'CODE', 'MDBG', 'RELA',
+                                    'PNTS', 'LNKS', 'PAD ', 'VOBJ',
+                                    'VMSK', 'UVSQ',
+                                    # Sound files
+                                    '.CTL', '.TBL', 'SEQS',
+                                    'STRG', 'BITM', 'FRMT', 'IMAG']:
+                        #breakpoint()
+                        length = int.from_bytes(fin.read(4), byteorder='big')
+                        # check for 'PAD '
+                        if length == 1346454560: # This is 'PAD ' as an integer
+                            padLength = int.from_bytes(fin.read(4), byteorder='big')
+                            sectionData = fin.read(padLength)
+                            print('  %s: 0x%06X:' % (magicStr + 'PAD ', padLength))
+                            hexdump(sectionData)
+                            fileOffset = fin.tell() - 8
+                        else:
+                            sectionData = fin.read(length)
+                            print('  %s: 0x%06X:' % (magicStr, length))
+                            hexdump(sectionData)
+
+                    elif magicStr == 'UVFT':
+                        #print('  %s' % magicStr)
+                        blockType = magicStr
+                        uvftSize = fin.read(4)
+                        uvftSizeInt = int.from_bytes(uvftSize, byteorder='big')
+                        uvftStr = uvftSize.decode('utf-8')
+                        if uvftSizeInt <= 52:
+                            length = uvftSizeInt
+                            sectionData = fin.read(length)
+                            print('  %s: 0x%06X:' % (magicStr, length))
+                            hexdump(sectionData)
+                        elif uvftStr == 'UVMO':
+                            length = int.from_bytes(fin.read(4), byteorder='big')
+                            sectionData = fin.read(length)
+                            print('  %s: 0x%06X:' % (magicStr + uvftStr, length))
+                            hexdump(sectionData)
+                            #breakpoint()
+                            fileOffset = fin.tell() - 4
+                        elif uvftStr == 'PAD ':
+                            length = int.from_bytes(fin.read(4), byteorder='big')
+                            sectionData = fin.read(length)
+                            print('  %s: 0x%06X:' % (magicStr + uvftStr, length))
+                            hexdump(sectionData)
+                    elif magicStr == 'MODU':
+                        padcheck = fin.read(4)
+                        padStr = padcheck.decode('utf-8')
+                        if padStr == 'PAD ':
+                            length = int.from_bytes(fin.read(4), byteorder='big')
+                            sectionData = fin.read(length)
+                            print('  %s: 0x%06X:' % (magicStr + padStr, length))
+                            hexdump(sectionData)
+                    elif magicStr == 'GZIP': # not actually gzip, but MIO0 container
+                        gzipLength = int.from_bytes(fin.read(4), byteorder='big')
+                        absOffset = fin.tell() + gzipLength
+                        # GZIP has extra COMM header
+                        decompType = fin.read(4)
+                        decompTypeStr = decompType.decode('utf-8')
+                        decompLength = int.from_bytes(fin.read(4), byteorder='big')
+                        #print('  %s: 0x%06X:' % (decompTypeStr + decompLength))
+                        
+                        compBytes = fin.read(gzipLength - 8)
+                        decompBytes = decompress_mio0(compBytes)
+
+                        print('  %s: 0x%06X: %s' % (magicStr, gzipLength, decompTypeStr))
+                        hexdump(decompBytes)
+            else:
+                print('unknown top magic: ' + ''.join('{:02x}'.format(x) for x in magic) + ' "' + magicStr + '"')
+                dumping = False
+                return
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='Pilotwings 64 File System Dumper')
+    ap.add_argument('file', help='File path of input')
+    ap.add_argument('-s', '--start', dest='startOffset', type=auto_int, default=0x0DF5B0, help='Start offset of file system')
+    ap.add_argument('-x', '--hex', dest='hexSize', type=auto_int, default=0x60, help='Size of hexdump for unparsed sections')
+    args = ap.parse_args()
+    pw64_dump_filesys(args.file, args.startOffset, args.hexSize)
